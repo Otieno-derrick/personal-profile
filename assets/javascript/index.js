5e092408f125801f76cdcb10a177a187088a11de
derrique <otieno.derrick@student.moringaschool.com>
2022-06-06 17:39:56 +0300

    adding images

---
+++ assets/javascript/index.js
@@ -0,0 +1,15 @@
+let slides = document.querySelectorAll('.smooth-slides');
+
+for (let slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses();
+
+        slide.classList.add('active');
+    })
+}
+
+function clearActiveClasses() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    })
+}
